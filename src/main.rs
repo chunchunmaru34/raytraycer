8e262e1fe567a9655bda2ce8126cc445a3c24803
Aliaksei Labkovich <aliaksei.labkovich@itechart-group.com>
2019-10-15 08:34:33 +0300

    Attempt to use vectors to calculate intersection

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,66 @@
 extern crate image;
 
 mod sphere;
-use sphere::{Sphere, Point};
+use sphere::{Sphere, Point, Light, Ray, Vec3};
 use image::{ImageBuffer};
 
 fn main() {
     render();
 }
 
+// fn render() {
+//     let coordinates = Point::new(150.0, 400.0, 32.0);
+//     let radius = 120.0;
+//     let color = (12 as u8, 55 as u8, 44 as u8);
+//     let test_sphere = Sphere::new(coordinates, radius, color);
+
+//     let coordinates2 = Point::new(350.0, 200.0, 12.0);
+//     let radius2 = 76.0;
+//     let color2 = (225 as u8, 12 as u8, 225 as u8);
+//     let test_sphere2 = Sphere::new(coordinates2, radius2, color2);
+
+//     let light = Light::new(Point::new(500., 500., 150.), 20.);
+
+//     let img = ImageBuffer::from_fn(512, 512, |x, y| {
+//         let direction = Point::new(x as f32, y as f32, 100.0);
+//         let origin = Point::new(x as f32, y as f32, 0.);
+//         let ray = Ray::new(origin, direction);
+
+//         let mut pixel = image::Rgb([185, 185, 185]);
+
+//         for sphere in vec![&test_sphere, &test_sphere2] {
+//             if sphere.ray_intersect(&ray) {
+//                 pixel = image::Rgb([sphere.color.0, sphere.color.1, sphere.color.2])
+//             }
+//         }
+
+//         return pixel;
+//     });
+
+//     img.save("test.png").unwrap();
+// }
+
 fn render() {
-    let coordinates = Point::new(150.0, 400.0, 32.0);
-    let radius = 120.0;
-    let color = (12 as u8, 55 as u8, 44 as u8);
-    let test_sphere = Sphere::new(coordinates, radius, color);
+    let coordinates = Vec3::new(10.0, 10.0, 12.0);
+    let dark_green = (12 as u8, 55 as u8, 44 as u8);
+    let test_sphere = Sphere::new(coordinates, 50.0, dark_green);
 
-    let coordinates2 = Point::new(350.0, 200.0, 12.0);
-    let radius2 = 76.0;
-    let color2 = (225 as u8, 12 as u8, 225 as u8);
-    let test_sphere2 = Sphere::new(coordinates2, radius2, color2);
+    let coordinates2 = Vec3::new(150.0, 100.0, 12.0);
+    let pink = (225 as u8, 12 as u8, 225 as u8);
+    let test_sphere2 = Sphere::new(coordinates2, 50.0, pink);
 
+    let light = Light::new(Point::new(500., 500., 150.), 20.);
 
     let img = ImageBuffer::from_fn(512, 512, |x, y| {
-        let direction = Point::new(x as f32, y as f32, 100.0);
-        let origin = Point::new(x as f32, y as f32, 0.);
+        let direction = Vec3::new(x as f32, y as f32, 1.).normalize();
+        let origin = Vec3::new(x as f32, y as f32, 0.);
+        // let origin = Vec3::new(256., 256., 0.);
+        let mut ray = Ray::new(origin, direction, std::f32::MAX);
+
         let mut pixel = image::Rgb([185, 185, 185]);
 
         for sphere in vec![&test_sphere, &test_sphere2] {
-            if sphere.ray_intersect(&origin, &direction) {
+            if sphere.ray_intersect(&mut ray) {
                 pixel = image::Rgb([sphere.color.0, sphere.color.1, sphere.color.2])
             }
         }
@@ -35,15 +69,4 @@ fn render() {
     });
 
     img.save("test.png").unwrap();
-}
-
-struct Light {
-    pub position: Point,
-    pub intensity: f32,
-}
-
-impl Light {
-    pub fn new(position: Point, intensity: f32) -> Self {
-        Self { position, intensity }
-    }
 }
--- src/sphere.rs
+++ src/sphere.rs
@@ -11,36 +11,147 @@ impl Point {
 }
 
 pub struct Sphere {
-    pub center: Point,
+    pub center: Vec3,
     pub radius: f32,
     pub color: (u8, u8, u8),
 }
 
 impl Sphere {
-    pub fn new(center: Point, radius: f32, color: (u8, u8, u8)) -> Self {
+    pub fn new(center: Vec3, radius: f32, color: (u8, u8, u8)) -> Self {
         Self { center, radius, color }
     }
 
-    pub fn ray_intersect(&self, orig: &Point, dir: &Point) -> bool {
-        // orig - A
-        // dir - B
-        // center - C
-        let ab = [(orig.x - dir.x).abs(), (orig.y - dir.y).abs(), (orig.z - dir.z).abs()];
-        let ac = [(orig.x - self.center.x).abs(), (orig.y - self.center.y).abs(), (orig.z - self.center.z).abs()];
-        let cb = [(dir.x - self.center.x).abs(), (dir.y - self.center.y).abs(), (dir.z - self.center.z).abs()];
+    // pub fn ray_intersect(&self, ray: &Ray) -> bool {
+    //     let orig = &ray.origin;
+    //     let dir = &ray.direction;
+    //     // orig - A
+    //     // dir - B
+    //     // center - C
+    //     let ab = [(orig.x - dir.x).abs(), (orig.y - dir.y).abs(), (orig.z - dir.z).abs()];
+    //     let ac = [(orig.x - self.center.x).abs(), (orig.y - self.center.y).abs(), (orig.z - self.center.z).abs()];
+    //     let cb = [(dir.x - self.center.x).abs(), (dir.y - self.center.y).abs(), (dir.z - self.center.z).abs()];
 
-        let ab_module = (ab[0].powi(2) + ab[1].powi(2) + ab[2].powi(2)).sqrt();
-        let ac_module = (ac[0].powi(2) + ac[1].powi(2) + ac[2].powi(2)).sqrt();
-        let cb_module = (cb[0].powi(2) + cb[1].powi(2) + cb[2].powi(2)).sqrt();
+    //     let ab_module = (ab[0].powi(2) + ab[1].powi(2) + ab[2].powi(2)).sqrt();
+    //     let ac_module = (ac[0].powi(2) + ac[1].powi(2) + ac[2].powi(2)).sqrt();
+    //     let cb_module = (cb[0].powi(2) + cb[1].powi(2) + cb[2].powi(2)).sqrt();
        
-        let cos_alpha = (ac_module.powi(2) + ab_module.powi(2) - cb_module.powi(2)) / (2.0 * ab_module * ac_module);
-        if cos_alpha < 0.0 {
+    //     let cos_alpha = (ac_module.powi(2) + ab_module.powi(2) - cb_module.powi(2)) / (2.0 * ab_module * ac_module);
+    //     if cos_alpha < 0.0 {
+    //         return false;
+    //     }
+    //     let projection = ac_module * cos_alpha;
+
+    //     let distance = (ac_module.powi(2) - projection.powi(2)).abs().sqrt();
+        
+    //     return distance <= self.radius;
+    // }
+
+    pub fn ray_intersect(&self, ray: &mut Ray) -> bool {
+        let orig = &ray.origin;
+        let dir = &ray.direction;
+        let c = self.center.minus(orig);
+
+        let tca = c.dot_product(dir);
+        if tca < 0. {
             return false;
         }
-        let projection = ac_module * cos_alpha;
 
-        let distance = (ac_module.powi(2) - projection.powi(2)).abs().sqrt();
+        let projection = c.dot_product(&c) - tca * tca;
+
+        if projection > self.radius * self.radius {
+            return false;
+        }
+
+        let thc = (self.radius * self.radius - projection).sqrt();
+        let mut t0 = tca - thc;
+        let t1 = tca + thc;
         
-        return distance <= self.radius;
+        if t0 < 0. {
+            t0 = t1;
+        }
+
+        if t0 < 0. {
+            return false;
+        }
+
+        ray.t = t0;
+
+        return true;
+    }
+}
+
+pub struct Light {
+    pub position: Point,
+    pub intensity: f32,
+}
+
+impl Light {
+    pub fn new(position: Point, intensity: f32) -> Self {
+        Self { position, intensity }
+    }
+}
+
+pub struct Ray {
+    pub origin: Vec3,
+    pub direction: Vec3,
+    pub t: f32,
+}
+
+impl Ray {
+    pub fn new(origin: Vec3, direction: Vec3, t: f32) -> Self {
+        Self { origin, direction, t }
+    }
+}
+
+pub struct Vec3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl Vec3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Self { x, y, z }
+    }
+
+    pub fn dot_product(&self, vec: &Vec3) -> f32 {
+        self.x * vec.x + self.y * vec.y  + self.z * vec.z
+    }
+
+    pub fn scale(&self, factor: f32) -> Self {
+        Self {
+            x: self.x * factor,
+            y: self.y * factor,
+            z: self.z * factor,
+        }
+    }
+
+    pub fn minus(&self, vec: &Vec3) -> Self {
+        Self {
+            x: self.x - vec.x,
+            y: self.y - vec.y,
+            z: self.z - vec.y,
+        }
+    }
+
+    pub fn plus(&self, vec: &Vec3) -> Self {
+        Self {
+            x: self.x - vec.x,
+            y: self.y - vec.y,
+            z: self.z - vec.y,
+        }
+    }
+
+    pub fn length(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2) + self.z.powi(2)).sqrt()
+    }
+
+    pub fn normalize(&self) -> Self {
+        let del = 1. / self.length();
+        Self {
+            x: self.x / del,
+            y: self.y / del,
+            z: self.z / del,
+        }
     }
 }
